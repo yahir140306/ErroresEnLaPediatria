5817da2d13d1988a0b588465a6ed2eb8c7be782f
yahir140306 <joseyahirhernandez11@gmail.com>
2023-11-21 00:21:44 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('#nav-menu');
+ScrollReveal().reveal('.one', { delay: 500 });
+ScrollReveal().reveal('.two', { delay: 800 });
+ScrollReveal().reveal('.footer-section', { delay: 1000 });
